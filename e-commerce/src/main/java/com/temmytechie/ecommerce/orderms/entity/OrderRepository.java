eaed4e0e075f434349906aecbfa307eba803abb4
Temmy Techie <91670087+Temmytechguy@users.noreply.github.com>
2023-12-19 02:38:40 +0100

    Add files via upload

---
+++ e-commerce/src/main/java/com/temmytechie/ecommerce/orderms/entity/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.temmytechie.ecommerce.orderms.entity;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author TemmyTechie
+ */
+
+public interface OrderRepository extends CrudRepository<Order, Long>{
+
+}
